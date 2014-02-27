cd82b1198fff52c0269f6c7009a886b707d484f1
Grimimi <grimfw@gmail.com>
2014-02-27 05:07:28 +0100

    initialisation du module graphics

---
+++ sources/Graphics.rb
@@ -0,0 +1,363 @@
+=begin
+ Event Extender 5
+ Graphics Module
+ Access to graphics components
+ ===============================================================================
+ http://www.biloucorp.com
+ ===============================================================================
+ By Grim 
+=end
+
+#==============================================================================
+# ** Game_Picture
+#------------------------------------------------------------------------------
+#  This class handles pictures. It is created only when a picture of a specific
+# number is required internally for the Game_Pictures class.
+#==============================================================================
+
+class Game_Picture
+  #--------------------------------------------------------------------------
+  # * Alias
+  #--------------------------------------------------------------------------
+  alias :ee_gfx_initialize  :initialize
+  alias :ee_gfx_show        :show
+  alias :ee_gfx_update      :update
+  #--------------------------------------------------------------------------
+  # * Public Instance Variables
+  #--------------------------------------------------------------------------
+  attr_accessor  :number                   # picture index
+  attr_accessor  :name                     # filename
+  attr_accessor  :origin                   # starting point
+  attr_accessor  :x                        # x-coordinate
+  attr_accessor  :y                        # y-coordinate
+  attr_accessor  :zoom_x                   # x directional zoom rate
+  attr_accessor  :zoom_y                   # y directional zoom rate
+  attr_accessor  :opacity                  # opacity level
+  attr_accessor  :blend_type               # blend method
+  attr_accessor  :tone                     # color tone
+  attr_accessor  :angle                    # rotation angle
+  attr_accessor  :pin
+  attr_accessor  :shake
+  attr_accessor  :mirror
+  attr_accessor  :wave_amp
+  attr_accessor  :wave_speed
+  #--------------------------------------------------------------------------
+  # * Object Initialization
+  #--------------------------------------------------------------------------
+  def initialize(number)
+    ee_gfx_initialize(number)
+    clear_effects
+  end
+  #--------------------------------------------------------------------------
+  # * Clear effects
+  #--------------------------------------------------------------------------
+  def clear_effects
+    @mirror = false
+    @wave_amp = @wave_speed = 0
+    @pin = false
+    clear_shake
+  end
+  #--------------------------------------------------------------------------
+  # * Show Picture
+  #--------------------------------------------------------------------------
+  def show(name, origin, x, y, zoom_x, zoom_y, opacity, blend_type)
+    ee_gfx_show(name, origin, x, y, zoom_x, zoom_y, opacity, blend_type)
+    clear_effects
+  end
+  #--------------------------------------------------------------------------
+  # * Clear Shake
+  #--------------------------------------------------------------------------
+  def clear_shake
+    @shake_power = 0
+    @shake_speed = 0
+    @shake_duration = 0
+    @shake_direction = 1
+    @shake = 0
+  end
+  #--------------------------------------------------------------------------
+  # * Start Shaking
+  #     power: intensity
+  #     speed: speed
+  #--------------------------------------------------------------------------
+  def start_shake(power, speed, duration)
+    @shake_power = power
+    @shake_speed = speed
+    @shake_duration = duration
+  end
+  #--------------------------------------------------------------------------
+  # * Update Shake
+  #--------------------------------------------------------------------------
+  def update_shake
+    if @shake_duration > 0 || @shake != 0
+      delta = (@shake_power * @shake_speed * @shake_direction) / 10.0
+      if @shake_duration <= 1 && @shake * (@shake + delta) < 0
+        @shake = 0
+      else
+        @shake += delta
+      end
+      @shake_direction = -1 if @shake > @shake_power * 2
+      @shake_direction = 1 if @shake < - @shake_power * 2
+      @shake_duration -= 1
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Frame Update
+  #--------------------------------------------------------------------------
+  def update
+    ee_gfx_update
+    update_shake
+  end
+end
+
+#==============================================================================
+# ** Sprite_Picture
+#------------------------------------------------------------------------------
+#  This sprite is used to display pictures. It observes an instance of the
+# Game_Picture class and automatically changes sprite states.
+#==============================================================================
+
+class Sprite_Picture
+  #--------------------------------------------------------------------------
+  # * Alias
+  #--------------------------------------------------------------------------
+  alias ee_gfx_update update
+  alias ee_gfx_update_origin update_origin
+  #--------------------------------------------------------------------------
+  # * Get cache
+  #--------------------------------------------------------------------------
+  def swap_cache(name)
+    if /^(\/Pictures|Pictures)\/(.*)/ =~ name
+      return Cache.picture($2)
+    end
+    if /^(\/Battlers|Battlers)\/(.*)/ =~ name
+      return Cache.battler($2, 0)
+    end
+    if /^(\/Battlebacks1|Battlebacks1)\/(.*)/ =~ name
+      return Cache.battleback1($2)
+    end
+    if /^(\/Battlebacks2|Battlebacks2)\/(.*)/ =~ name
+      return Cache.battleback2($2)
+    end
+    if /^(\/Parallaxes|Parallaxes)\/(.*)/ =~ name
+      return Cache.parallax($2)
+    end
+    if /^(\/Titles1|Titles1)\/(.*)/ =~ name
+      return Cache.title1($2)
+    end
+    if /^(\/Titles2|Titles2)\/(.*)/ =~ name
+      return Cache.title2($2)
+    end
+    return Cache.picture(name)
+  end
+  #--------------------------------------------------------------------------
+  # * Update Transfer Origin Bitmap
+  #--------------------------------------------------------------------------
+  def update_bitmap
+    if @picture.name.empty?
+      self.bitmap = nil
+    else
+      self.bitmap = swap_cache(@picture.name)
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Frame Update
+  #--------------------------------------------------------------------------
+  def update
+    ee_gfx_update
+    self.mirror = !self.mirror if @picture.mirror != self.mirror
+    self.wave_amp = @picture.wave_amp if @picture.wave_amp != self.wave_amp
+    self.wave_speed = @picture.wave_speed if @picture.wave_speed != self.wave_speed
+  end
+  #--------------------------------------------------------------------------
+  # * Update Position
+  #--------------------------------------------------------------------------
+  def update_position
+    if @picture.pin
+      self.x = @picture.x - ($game_map.display_x * 32) + @picture.shake
+      self.y = @picture.y - ($game_map.display_y * 32)
+    else
+      self.x = @picture.x + @picture.shake
+      self.y = @picture.y
+    end
+    self.z = @picture.number
+  end
+  #--------------------------------------------------------------------------
+  # * Update Origin
+  #--------------------------------------------------------------------------
+  def update_origin
+    if @picture.origin.is_a?(Array)
+      x, y = @picture.origin
+      self.ox, self.oy = x, y
+    else
+      ee_gfx_update_origin
+    end
+  end
+end  
+
+#==============================================================================
+# ** API
+#------------------------------------------------------------------------------
+#  Insert command
+#==============================================================================
+
+module GFXApi
+  #--------------------------------------------------------------------------
+  # * Private Commands
+  #--------------------------------------------------------------------------
+  def screen; $game_party.in_battle ? $game_troop.screen : $game_map.screen end
+  def pictures; screen.pictures; end
+  private :screen, :pictures
+  #--------------------------------------------------------------------------
+  # * Pictures commands
+  #--------------------------------------------------------------------------
+  #--------------------------------------------------------------------------
+  # * Show a picture
+  # Origin : 0 | 1 (0 = Corner High Left, 1 = Center)
+  # Blend_type : 0 (normal)
+  #--------------------------------------------------------------------------
+  def picture_show(id, n, x=0, y=0, ori=0,  z_x=100, z_y=100, op=255, bl=0)
+    pictures[id].show(n, ori, x, y, z_x, z_y, op, bl)
+  end
+  #--------------------------------------------------------------------------
+  # * Modify Origin
+  # Origin : 0 | 1 (0 = Corner High Left, 1 = Center)
+  #--------------------------------------------------------------------------
+  def picture_origin(id, *origin)
+    origin = origin[0] if origin.length == 1
+    pictures[id].origin = origin
+  end
+  #--------------------------------------------------------------------------
+  # * Modify x position
+  #--------------------------------------------------------------------------
+  def picture_x(id, x=false)
+    return pictures[id].x unless x
+    pictures[id].x = x
+  end
+  #--------------------------------------------------------------------------
+  # * Modify y position
+  #--------------------------------------------------------------------------
+  def picture_y(id, y=false)
+    return pictures[id].y unless y
+    pictures[id].y = y
+  end
+  #--------------------------------------------------------------------------
+  # * Modify position
+  #--------------------------------------------------------------------------
+  def picture_position(id, x, y)
+    picture_x(id, x)
+    picture_y(id, y)
+  end
+  #--------------------------------------------------------------------------
+  # * Move picture
+  #--------------------------------------------------------------------------
+  def picture_move(id, x, y, zoom_x, zoom_y, duration, opacity=-1, blend_type=-1, origin=-1)
+    picture = pictures[id]
+    opacity = (opacity == -1) ? picture.opacity : opacity
+    blend_type = (blend_type == -1) ? picture.blend_type : blend_type
+    origin = (origin == -1) ? picture.origin : origin
+    picture.move(origin, x, y, zoom_x, zoom_y, opacity, blend_type, duration)
+  end
+  #--------------------------------------------------------------------------
+  # * Modify wave
+  #--------------------------------------------------------------------------
+  def picture_wave(id, amp, speed)
+    pictures[id].wave_amp = amp
+    pictures[id].wave_speed = speed
+  end
+  #--------------------------------------------------------------------------
+  # * Apply Mirror
+  #--------------------------------------------------------------------------
+  def picture_flip(id)
+    pictures[id].mirror = !pictures[id].mirror
+  end
+  #--------------------------------------------------------------------------
+  # * Modify Angle
+  #--------------------------------------------------------------------------
+  def picture_angle(id, angle=false)
+    return pictures[id].angle unless angle
+    pictures[id].angle = angle%360
+  end
+  #--------------------------------------------------------------------------
+  # * Rotate
+  #--------------------------------------------------------------------------
+  def picture_rotate(id, speed)
+    pictures[id].rotate(speed)
+  end
+  #--------------------------------------------------------------------------
+  # * change Zoom X
+  #--------------------------------------------------------------------------
+  def picture_zoom_x(id, zoom_x=false)
+    return pictures[id].zoom_x unless zoom_x
+    pictures[id].zoom_x = zoom_x
+  end
+  #--------------------------------------------------------------------------
+  # * change Zoom Y
+  #--------------------------------------------------------------------------
+  def picture_zoom_y(id, zoom_y=false)
+    return pictures[id].zoom_y unless zoom_y
+    pictures[id].zoom_y = zoom_y
+  end
+  #--------------------------------------------------------------------------
+  # * change Zoom
+  #--------------------------------------------------------------------------
+  def picture_zoom(id, zoom_x, zoom_y = -1)
+    zoom_y = zoom_x if zoom_y == -1
+    picture_zoom_x(id, zoom_x)
+    picture_zoom_y(id, zoom_y)
+  end
+  #--------------------------------------------------------------------------
+  # * change Tone
+  #--------------------------------------------------------------------------
+  def picture_tone(id, *args)
+    case args.length
+    when 1; tone = args[0]
+    else
+      r, g, b = args[0], args[1], args[2]
+      gray = args[3] || 0
+      tone = Tone.new(r, g, b, gray)
+    end
+    pictures[id].tone = tone
+  end
+  #--------------------------------------------------------------------------
+  # * Change blend type
+  #--------------------------------------------------------------------------
+  def picture_blend(id, blend)
+    blend_type = 0
+    blend_type = blend if [0,1,2].include?(blend)
+    pictures[id].blend_type = blend_type
+  end
+  #--------------------------------------------------------------------------
+  # * Pin picture on the map
+  #--------------------------------------------------------------------------
+  def picture_pin(id)
+    pictures[id].pin = true
+  end
+  #--------------------------------------------------------------------------
+  # * Pin picture on the map
+  #--------------------------------------------------------------------------
+  def picture_detach(id)
+    pictures[id].pin = false
+  end
+  #--------------------------------------------------------------------------
+  # * Change Picture Opacity
+  #--------------------------------------------------------------------------
+  def picture_opacity(id, value)
+    pictures[id].opacity = value
+  end
+  #--------------------------------------------------------------------------
+  # * Shake the picture
+  #--------------------------------------------------------------------------
+  def picture_shake(id, power, speed, duration)
+    pictures[id].start_shake(power, speed, duration)
+  end
+  #--------------------------------------------------------------------------
+  # * Erase a picture
+  #--------------------------------------------------------------------------
+  def picture_erase(id)
+    pictures[id].erase
+  end
+  #--------------------------------------------------------------------------
+  # * Load commands
+  #--------------------------------------------------------------------------
+  append_commands
+end
